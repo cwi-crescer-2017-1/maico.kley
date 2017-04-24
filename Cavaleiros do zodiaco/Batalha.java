3040fb6f8b92dbc4cb295115a2cf52c281a3946f
Maico Kley <maico.kley@cwi.com.br>
2017-04-24 17:22:17 -0300

    feat(Batalha): implementação da batalha por turnos e seus devidos testes.

--- Cavaleiros do zodiaco/Batalha.java
+++ Cavaleiros do zodiaco/Batalha.java
@@ -8,14 +8,31 @@ public class Batalha
        this.saint2 = saint2;
     }
     
-    public void iniciar()throws Exception{
+    public void iniciar() {
         int valor1 = this.saint1.getArmadura().getCategoria().getValor();
-        int valor2 = this.saint2.getArmadura().getCategoria().getValor();        
+        int valor2 = this.saint2.getArmadura().getCategoria().getValor();
+        final double dano = 10;
+        Saint saintEmAcao = null;
 
-        if (valor1 >= valor2){
-            this.saint1.getProximoMovimento();
+        if (valor1 >= valor2) {
+            saintEmAcao = this.saint1;
+            this.saint2.perderVida(dano);
         } else {
-            this.saint2.getProximoMovimento();
+            saintEmAcao = this.saint2;
+            this.saint1.perderVida(dano);
         }
-    }
+
+        boolean nenhumMorto = true;
+        while (nenhumMorto) {
+            // 1. definindo quem vai atuar no round
+            saintEmAcao = saintEmAcao == this.saint1 ? this.saint2 : this.saint1;
+            // 2. executando próximo movimento
+            Movimento proximoMovimento = saintEmAcao.getProximoMovimento();
+            proximoMovimento.executar();
+            // 3. verificando se a batalha acabou
+            nenhumMorto = this.saint1.getStatus() != Status.MORTO &&
+            this.saint2.getStatus() != Status.MORTO;
+        }
+
+    }    
 }
