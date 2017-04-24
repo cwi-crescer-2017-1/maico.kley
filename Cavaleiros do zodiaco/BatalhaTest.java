90cafa19769ab782a424361f517e74072eeee3d6
Maico Kley <maico.kley@cwi.com.br>
2017-04-24 17:22:17 -0300

    feat(Batalha): implemetação da batalha por turnos e seus devidos testes.

--- Cavaleiros do zodiaco/BatalhaTest.java
+++ Cavaleiros do zodiaco/BatalhaTest.java
@@ -6,42 +6,55 @@ import org.junit.Test;
 public class BatalhaTest
 {
     @Test
-    public void batalhaPrimeiroCategoriaMaisAlta()throws Exception{        
-        Saint saint1 = new GoldSaint("Shaka", "Virgem");
-        Saint saint2 = new SilverSaint("Orfeu", "Lira");
-        
-        Batalha batalha = new Batalha(saint1, saint2);
-        
+    public void categoriaSaint1MaiorQueSaint2() throws Exception {
+        // Arrange
+        Saint shaina = new SilverSaint("Shaina", "Serpente");
+        Saint hyoga = new BronzeSaint("Hyoga", "Cisne");
+        Batalha batalha = new Batalha(shaina, hyoga);
+        shaina.aprenderGolpe(new Golpe("Venha Cobra", 5));
+        shaina.golpear(hyoga);
+        hyoga.aprenderGolpe(new Golpe("Pó de diamante", 4));
+        hyoga.golpear(shaina);
+        // Act
         batalha.iniciar();
-        
-        assertEquals(90.0f, saint2.getVida(),0);
-        assertEquals(100, saint1.getVida(),0);
+        // Assert
+        assertEquals(28, shaina.getVida(), 0.01);
+        assertEquals(0, hyoga.getVida(), 0.01);
     }
-    
+
     @Test
-    public void batalhaSegundoCategoriaMaisAlta()throws Exception{        
-        Saint saint1 = new SilverSaint("Orfeu", "Lira");
-        Saint saint2 = new GoldSaint("Shaka", "Virgem");        
-        
-        Batalha batalha = new Batalha(saint1, saint2);
-        
+    public void categoriasIguaisSaint2PerdeVida() throws Exception {
+        // Arrange
+        Saint aldebaran = new GoldSaint("Aldebaran", "Touro");
+        Saint mascaraMorte = new GoldSaint("Máscara da Morte", "Câncer");
+        aldebaran.aprenderGolpe(new Golpe("Grande Chifre", 10));
+        aldebaran.adicionarMovimento(new VestirArmadura(aldebaran));
+        aldebaran.golpear(mascaraMorte);
+        mascaraMorte.aprenderGolpe(new Golpe("Ondas do Inferno", 10));
+        mascaraMorte.golpear(aldebaran);
+        Batalha batalha = new Batalha(mascaraMorte, aldebaran);
+        // Act
         batalha.iniciar();
-        
-        assertEquals(90.0f, saint1.getVida(),0);
-        assertEquals(100, saint2.getVida(),0);
+        // Assert
+        assertEquals(40, aldebaran.getVida(), 0.01);
+        assertEquals(0, mascaraMorte.getVida(), 0.01);
     }
-    
+
     @Test
-    public void batalhaMesmaCategoria()throws Exception{        
-        Saint saint1 = new BronzeSaint("Shiryu", "Dragão");
-        Saint saint2 = new BronzeSaint("Seiya", "Pegasus");        
-        
-        Batalha batalha = new Batalha(saint1, saint2);
-        
+    public void categoriaSaint2MaiorSaint1PerdeVida() throws Exception {
+        // Arrange
+        Saint ikki = new BronzeSaint("Ikki", "Fênix");
+        Saint mascaraMorte = new GoldSaint("Máscara da Morte", "Câncer");
+        ikki.aprenderGolpe(new Golpe("Ave Fênix", 15));
+        ikki.adicionarMovimento(new VestirArmadura(ikki));
+        ikki.golpear(mascaraMorte);
+        mascaraMorte.aprenderGolpe(new Golpe("Ondas do Inferno", 10));
+        mascaraMorte.golpear(ikki);
+        Batalha batalha = new Batalha(ikki, mascaraMorte);
+        // Act
         batalha.iniciar();
-        
-        assertEquals(90.0f, saint2.getVida(),0);
-		assertEquals(100, saint1.getVida(),0);
-        
+        // Assert
+        assertEquals(20, ikki.getVida(), 0.01);
+        assertEquals(0, mascaraMorte.getVida(), 0.01);
     }
 }
