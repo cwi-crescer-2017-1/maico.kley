0c5ca17b167b6deca45bfdd695666485853fac8c
Maico Kley <maico.kley@cwi.com.br>
2017-05-12 03:05:48 -0300

    feat(Modulo4): implementação de exercicios 1 a 3 referentes a lista 1 de javascript.

---
+++ modulo4/Lista1/lista-js-basico.js
@@ -0,0 +1,33 @@
+//Ex 1
+function daisyGame(x) {
+  if (typeof x === typeof 3) {
+    if (x%2==1) {
+      return "Love me"
+    } else if (x%2==0) {
+      return "Love me not";
+    }
+  }
+}
+//Ex 2
+function maiorTexto(x) {
+  var tamanho = x[0];
+  for (var i = 1; i < x.length; i++) {
+    if (x[0].length<x[i].length) {
+      tamanho = x[i];
+    }
+  }
+  return tamanho;
+}
+//Ex 3
+/*var calcular = function(funcao, a, b) {
+  //return funcao(arguments[0], arguments[1])
+  return typeof funcao === "function" && funcao(a, b);
+}*/
+function intrutorQuerido(x) {
+  for (var i = 0; i < x.length; i++) {
+    console.log('olá querido instrutor', x[i]);
+  }
+}
+function imprime(lista, funcao) {  
+  return typeof funcao === "function" && funcao(lista);
+}
