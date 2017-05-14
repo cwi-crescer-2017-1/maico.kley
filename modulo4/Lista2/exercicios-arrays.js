f00247262889f32961feb524c69a6daab16abff9
Maico Kley <maico.kley@cwi.com.br>
2017-05-13 21:34:45 -0300

    feat(Modulo4): implementação exercicio 1 referente a lista de exercicios 2.

---
+++ modulo4/Lista2/exercicios-arrays.js
@@ -0,0 +1,27 @@
+function seriesInvalidas(array) {
+  var retorno = "Séries inválidas: ";
+  for (let i in array) {
+    if (array[i].anoEstreia>2017) {
+      retorno += array[i].titulo + " - ";
+    }
+  }
+  return retorno;
+}
+
+function seriesInvalidas2(array) {
+  var retorno = "Séries inválidas: ";
+  for (let i in array) {
+    let verificacao = [array[i].anoEstreia, array[i].diretor, array[i].distribuidora,
+    array[i].elenco, array[i].genero, array[i].numeroEpisodios, array[i].temporadas, array[i].titulo]
+    if (array[i].anoEstreia>2017) {
+      retorno += array[i].titulo + " - ";
+    }
+    console.log(verificacao);
+    for(let j in verificacao){
+      if (verificacao[j] === null || verificacao[j] === undefined) {
+        retorno += array[i].titulo + " - ";
+      }
+    }
+  }
+  return retorno;
+}
