4f86e5d492f6584882989247a060a038c64e675f
Maico Kley <maico.kley@cwi.com.br>
2017-05-18 16:26:28 -0300

    refactor(Modulo5): exemplo 1 refatorado.

--- modulo5/dia2/exemplo.js
+++ modulo5/dia2/exemplo.js
@@ -2,6 +2,18 @@ var app = angular.module("container", []);
 app.controller("datas", function($scope) {
   $scope.clicar = clicar;
   function clicar() {
-    $scope.datavalida = new Date($scope.entrada);
+
+    if (angular.isUndefined($scope.entrada)) {
+      return;
+    }
+
+    let pattern = '/(\d{2})\/(\d{2})\/(\d{4})/';
+    let replace = '$1.$2.$3';
+    let dataFormatada = $scope.entrada.replace(pattern, replace);
+    console.log(dataFormatada);
+
+    let dataObjeto = new Date(dataFormatada);
+
+    $scope.datavalida = dataObjeto;
   };
 });
