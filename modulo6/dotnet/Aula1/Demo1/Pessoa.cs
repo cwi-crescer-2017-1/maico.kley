95ac34babd9c7b121515af69884d5f918be73638
Maico Kley <maico.kley@cwi.com.br>
2017-05-24 17:29:44 -0300

    feat(Modulo6): implementação de exemplos modulo 6.

---
+++ modulo6/dotnet/Aula1/Demo1/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Id { get; set; }
+        public DateTime Nascimento { get; set; }
+
+    }
+}
---
+++ modulo6/dotnet/Aula1/Demo1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> entradas = new List<int>();
+
+            bool continua = true;
+
+            while (continua)
+            {
+                var linhaLida = Console.ReadLine();
+                if(linhaLida == "exit")
+                {
+                    break;
+                }
+
+                entradas.Add(int.Parse(linhaLida));
+            }
+
+            foreach (var entrada in entradas)
+            {
+                Console.WriteLine(entrada);
+            }
+            Console.WriteLine("===FIM===");
+            Console.ReadKey();
+        }
+    }
+}
