ef0e201916abb1d71b2c8369992480eaa7182856
Maico Kley <maico.kley@cwi.com.br>
2017-05-25 10:34:30 -0300

    feat(Modulo6): inicio exercicio 1.

---
+++ modulo6/dotnet/Exercício1/folhaDePagamento/folhaDePagamento/GeradorDemonstrativo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace folhaDePagamento
+{
+    class GeradorDemonstrativo : IFolhaDePagamento
+    {
+        public Demonstrativo GerarDemonstrativo(int horasCategoria, double salarioBase, double horasExtras, double horasDescontadas)
+        {
+            double SalarioBase = salarioBase;
+            double HorasConvencao = horasCategoria;
+            double HorasExtras = horasExtras;
+            double HorasDescontadas = horasDescontadas;
+        }
+    }
+}
