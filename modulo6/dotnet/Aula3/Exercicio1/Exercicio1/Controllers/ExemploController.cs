30a137e16d37f405877168519b3042724eb064c6
Maico Kley <maico.kley@cwi.com.br>
2017-05-26 17:36:17 -0300

    feat(modulo6): dia 3.

---
+++ modulo6/dotnet/Aula3/Exercicio1/Exercicio1/Controllers/ExemploController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exercicio1.Controllers
+{
+    public class ExemploController : Controller
+    {
+        public List<string> Get()
+        {
+            var herois = new List<string>()
+        {
+            "Goku",
+            "Batman",
+            "Ryu"
+        };
+            return herois;
+        }
+    }    
+}
