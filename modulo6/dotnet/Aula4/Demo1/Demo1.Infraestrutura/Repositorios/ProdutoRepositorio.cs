e8c781452188103080019707a9331e229a3ffcbc
Maico Kley <maico.kley@cwi.com.br>
2017-05-29 17:26:09 -0300

    feat(Modulo6): dia 4 exemplo/inicio tema.

---
+++ modulo6/dotnet/Aula4/Demo1/Demo1.Infraestrutura/Repositorios/ProdutoRepositorio.cs
@@ -0,0 +1,69 @@
+﻿using Demo1.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1.Infraestrutura.Repositorios
+{
+    public class ProdutoRepositorio
+    {
+        string stringConexao = @"Server=13.65.101.67;User Id=maico.kley;Password=123456;Database=aluno09db";
+        public void Criar(Produto produto)
+        {            
+
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = @"insert into Produto (Nome, Preco, Estoque) values (@nome, @preco, @estoque);";
+
+                    comando.Parameters.AddWithValue("@nome", produto.Nome);
+                    comando.Parameters.AddWithValue("@preco", produto.Preco);
+                    comando.Parameters.AddWithValue("@estoque", produto.Estoque);
+
+                    comando.ExecuteNonQuery();
+                }
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = "SELECT @@IDENTITY";
+                    var result = (decimal)comando.ExecuteScalar();
+                    produto.Id = (int)result;
+                }
+            }
+        }
+
+        public List<Produto> Listar()
+        {
+            var produtos = new List<Produto>();
+
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = "SELECT Id, Nome, Preco, Estoque FROM Produto";
+
+                    var dataReader = comando.ExecuteReader();
+                    while(dataReader.Read())
+                    {
+                        var produto = new Produto();
+
+                        produto.Id = (int)dataReader["Id"];
+                        produto.Nome = (string)dataReader["Nome"];
+                        produto.Preco = (decimal)dataReader["Preco"];
+                        produto.Estoque = (int)dataReader["Estoque"];
+
+                        produtos.Add(produto);
+                    }
+                }
+            }
+            return produtos;
+        }
+    }
+}
