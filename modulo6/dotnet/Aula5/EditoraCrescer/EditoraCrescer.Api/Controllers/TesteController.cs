08caefc9d07ed2c0d87fdafa66343f6e608bef80
Maico Kley <maico.kley@cwi.com.br>
2017-06-02 17:21:20 -0300

    feat(Modulo6): dia 8.

---
+++ modulo6/dotnet/Aula5/EditoraCrescer/EditoraCrescer.Api/Controllers/TesteController.cs
@@ -0,0 +1,19 @@
+﻿using EditoraCrescer.Api.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EditoraCrescer.Api.Controllers
+{
+    [MinhaAutenticacao]
+    public class TesteController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
